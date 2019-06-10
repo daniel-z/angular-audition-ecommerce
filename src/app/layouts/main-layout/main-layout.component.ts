f8a982846eabd39e192cf4b04feb095133dac661
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-06-10 15:32:42 -0500

    Adding breadcrumbs

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainLayoutComponent implements OnInit {
 
+  links = [
+    'Home',
+    'Glasses'
+  ];
+
   constructor() {}
 
   heroImageUrl: string;
